5be9c59240d1c3d7a5f873d03d864de4e9812d3b
Irene Scott <irenejinscott@gmail.com>
2020-05-23 18:26:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ require "pry"
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 def create_project_hash
+projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
