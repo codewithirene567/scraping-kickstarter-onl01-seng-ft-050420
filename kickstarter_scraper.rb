d4ba61d1f1a6c44698a0c82cbd706b145e2a0963
Irene Scott <irenejinscott@gmail.com>
2020-05-23 18:26:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,12 +3,21 @@
 require "nokogiri"
 require "pry"
 
-def create_project_hash
+
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+def create_project_hash
+
+  kickstarter.css("li.project.grid_4").each do |project| 
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+  projects
 end
 
+# line 11 with the loop is saying that each one of the projects has been put
+  #into an array and we are going to go over each of them and have the projects become a key, then put the title as a value after
+  #making each of them a symbol to put them into the hash of all projects
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
