52a5e95eb61460da8b22909a5fd68b599365bbba
Irene Scott <irenejinscott@gmail.com>
2020-05-23 17:43:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,4 +16,4 @@ create_project_hash
 
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
-#percent funded: project.css("ul.project-stats li.first.funded strong").text
+#percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
